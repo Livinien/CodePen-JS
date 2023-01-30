82c2b1cab46c379d6ef38e10721de88aea947b52
Flavien <livinien@hotmail.fr>
2023-01-30 09:59:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+
+<!-- PARTIE 1 - TROIS TEXTAREA HTML / CSS / JAVASCRIPT  -->
+
+- Faire les textarea pour le HTML / CSS et JAVASCRIPT.
+- Régler la taille des textarea.
+- Enlever les balises dans les textarea.
+- Afficher le contenu qui est présent dans les textarea.
+- Ajouter le style.
+
+
+
+<!-- PARTIE 2 -->
+
+- Ecrire "Editor HTML & CSS".
+- Creéation de deux boutons pour implanter la balise de titre.
+- Création du textarea.
+- Afficher le "Live Coding" avec l'ajout de l'animation du loader.
+- Afficher le contenu qui est présent dans le textarea.
---
+++ app.js
@@ -0,0 +1,135 @@
+
+// AFFICHER LE MESSAGE EN DESSOUS DES TEXTAREA
+
+const textarea = document.querySelector(".field");
+
+textarea.addEventListener("keyup", () => {
+
+    document.querySelector("#title").innerHTML = textarea.value;
+});
+
+
+
+
+// AFFICHER LE STYLE DU TEXTE AVEC LE CSS
+
+const styleCss = document.querySelector(".field2");
+
+styleCss.addEventListener("keyup", () => {
+
+    document.querySelector("#styleCss").innerText = styleCss.value;
+});
+
+
+
+
+// AFFICHER LE STYLE DU TEXTE AVEC LE JAVASCRIPT
+
+const styleJs = document.querySelector(".field3");
+
+
+styleJs.addEventListener("keyup", () => {
+
+    document.querySelector("#styleJs").innerText = styleJs.value;
+});
+
+
+function run() {
+
+    new Function(styleJs.value)();
+}
+
+
+
+
+// AFFICHER BALISE H1 DANS LE TEXTAREA DE "EDITOR HTML ET CSS"
+
+const buttonB = document.querySelector(".button1");
+
+function addTitle() {
+
+    if(document.querySelector(".field4").innerHTML === ``) {
+        return document.querySelector(".field4").innerHTML = `<h1></h1>`;
+
+    } else(document.querySelector(".field4").innerHTML = ``);
+};
+
+buttonB.addEventListener("click", addTitle);
+
+
+
+
+
+// AFFICHER LE MESSAGE EN DESSOUS DU TITRE "LIVE CODING" - AFFICHAGE DE L'ANIMATION DU LOADER - AFFICHER LA PROGRESSE BAR
+
+const tallTextarea = document.querySelector(".field4");
+
+const loader = document.querySelector("#loader").style.display = "none";
+const progressBar = document.querySelector("#progress").style.display = "none";
+
+
+tallTextarea.addEventListener("keyup", () => {
+
+    document.querySelector("#title2").innerHTML = tallTextarea.value;
+
+    if(tallTextarea.value === "") {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("#progress").style.display = "none";
+       
+    } else if(tallTextarea != tallTextarea.value) {
+        document.querySelector("#loader").style.display = "block";
+        document.querySelector("#progress").style.display = "block";
+        console.log(tallTextarea.value.length);
+
+    };
+   
+});
+
+
+
+// AFFICHER LA PROGRESSION DE LA BAR EN POURCENTAGE EN FONCTION DU NOMBRE DE CARACTERE DANS LA TEXTAREA
+
+const numberCharacter = tallTextarea.value.length;
+
+const characterLimit = tallTextarea.length;
+
+tallTextarea.addEventListener("keyup", () => {
+
+    if(numberCharacter < 50) {
+        
+        document.querySelector("#progress").style.background = "#ff1515";
+        document.querySelector("#progress").style.width = "10%";
+        document.querySelector("#progress_bar").innerHTML = "10%";
+
+    } else if(numberCharacter < 51 && characterLimit > 100) {
+
+        document.querySelector("#progress").style.background = "#ff7000";
+        document.querySelector("#progress").style.width = "30%";
+        document.querySelector("#progress_bar").innerHTML = "30%";
+        console.log("30%");
+
+    } else if(numberCharacter < 101 && characterLimit > 150) {
+
+        document.querySelector("#progress").style.background = "#ff6400";
+        document.querySelector("#progress").style.width = "50%";
+        document.querySelector("#progress_bar").innerHTML = "50%";
+
+    } else if(numberCharacter < 151 && characterLimit > 170) {
+
+        document.querySelector("#progress").style.background = "#00e711";
+        document.querySelector("#progress").style.width = "70%";
+        document.querySelector("#progress_bar").innerHTML = "70%";
+
+    } else if(numberCharacter < 171 && characterLimit > 200) {
+
+        document.querySelector("#progress").style.background = "#00e711";
+        document.querySelector("#progress").style.width = "100%";
+        document.querySelector("#progress_bar").innerHTML = "100%";
+
+    } else {
+        document.querySelector("#progress").style.background = "#00e711";
+        document.querySelector("#progress_bar").innerHTML = "Vous avez atteint la limite de caractère dans ce champs";
+    }
+
+});
+
